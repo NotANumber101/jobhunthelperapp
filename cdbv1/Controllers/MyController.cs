ab5b842201f1e2179e0d8b7f28d89bdc821633c5
sergei <liddell.s@pm.me>
2026-04-10 22:59:52 -0700

    Refactor dbBuilder and MyQueries

--- cdbv1/Controllers/MyController.cs
+++ cdbv1/Controllers/MyController.cs
@@ -1,6 +1,7 @@
 using System;
 using cdbv1.Helpers;
 using cdbv1.Models;
+using cdbv1.Queries;
 using Npgsql;
 using Spectre.Console;
 
@@ -8,19 +9,19 @@ namespace cdbv1.Controllers;
 
 public class MyController()
 {
+    private static readonly string host = "localhost";
+    NpgsqlDataSourceBuilder dbBuilder = new DbSourceBuilder(host).Builder();
+
     public async Task<List<JobApplication>> GetAllApplications()
     {
         List<JobApplication> jobApplications = [];
         try
         {
-            DbInfoController dbIc = new();
-            var dbsb = new DbSourceBuilder("localhost");
-            await using var dataSource = dbsb.Builder().Build();
-
-
-
+            // var dbsb = new DbSourceBuilder(host);
+            // await using var dataSource = dbsb.Builder().Build();
+            await using var dataSource = dbBuilder.Build();
             AnsiConsole.MarkupLine("    -> [gray]Fetching applications...[/]");
-            await using (var cmd = dataSource.CreateCommand("SELECT * FROM application"))
+            await using (var cmd = dataSource.CreateCommand(MyQueries.GetAllApplicationsQuery()))
             await using (var reader = await cmd.ExecuteReaderAsync())
                 while (await reader.ReadAsync())
                 {
---
+++ cdbv1/Queries/MyQueries.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cdbv1.Queries
+{
+    // internal class?
+    public class MyQueries()
+
+    {
+        public string GetDbNames()
+        {
+            // Get all database names
+            return "SELECT datname FROM pg_database WHERE datistemplate = false;";
+        }
+        public static string GetAllApplicationsQuery()
+        {
+            return "SELECT * FROM application";
+        }
+        public string GetDbTableNamesSql()
+        {
+        // Get db table names and table fields 
+            return "SELECT table_name, table_type "
+                + "FROM information_schema.tables WHERE table_schema "
+                + "NOT IN ('pg_catalog', 'information_schema') "
+                + "ORDER BY table_schema, table_name;";
+        }
+        public string GetTableFieldNamesSql(string tableName)
+        {
+            return "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS "
+                + $"WHERE TABLE_NAME = '{tableName}' "
+                + "ORDER BY ORDINAL_POSITION;";
+        }
+        public string CreateNewDsaSolution(int problemId, string solution)
+        {
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            return $"INSERT INTO dsa_solution (problem_id, solution, date_completed) VALUES ({problemId}, '{solution}', '{today}') RETURNING id;";
+        }
+        public string UpdateDsaProblemDateCompletedTodayId(int problemId)
+        {
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            return $"UPDATE dsa_problem SET date_completed='{today}' WHERE id={problemId}";
+        }
+    }
+}
